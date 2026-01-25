d5839b292142e6408428c1480f04b93f50d68d72
Arijeet Roy <blakelabs57@gmail.com>
2026-01-25 22:45:52 +0530

    FIrsttest

--- firstTest.py
+++ firstTest.py
@@ -3,4 +3,15 @@ from selenium.webdriver.common.by import By
 
 driver= webdriver.Chrome()
 
-driver.get()
+driver.get("https://www.saucedemo.com/")
+
+title = driver.title
+
+driver.implicitly_wait(2)
+
+print("Page Title: ", title)
+
+username = driver.find_element(by=By.NAME, value="user-name")
+username.send_keys("standard_user")
+
+driver.quit()
